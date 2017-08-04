3778aca1a283e3d46f881435f2746e21ff1b399b
Gilga <gilgatx@gmail.com>
2017-08-04 12:16:00 +0200

    messages should work now

--- src/WEB-INF/js/messages-controller.js
+++ src/WEB-INF/js/messages-controller.js
@@ -9,7 +9,11 @@
 this.de_sb_messenger = this.de_sb_messenger || {};
 (function () {
 	var SUPER = de_sb_messenger.Controller;
-
+	var PERSONID = 0;
+	var SUBJECTID = 0;
+	var ISPERSON = false;
+	var ROOT = null;
+	
 	/**
 	 * Creates a new messages controller that is derived from an abstract controller.
 	 * @param sessionUser {de_sb_messenger.sessionUser} a reference for the session User
@@ -29,6 +33,9 @@ this.de_sb_messenger = this.de_sb_messenger || {};
 		var sessionUser = de_sb_messenger.APPLICATION.sessionUser;
 		if (!sessionUser) return;
 		SUPER.prototype.display.call(this);
+		PERSONID = sessionUser.identity
+		SUBJECTID = sessionUser.identity;
+		ISPERSON = true;
 
 		var subjectIdentities = [sessionUser.identity].concat(sessionUser.observedReferences);
 		var mainElement = document.querySelector("main");
@@ -37,8 +44,19 @@ this.de_sb_messenger = this.de_sb_messenger || {};
 
 		mainElement.appendChild(subjectsElement);
 		mainElement.appendChild(messagesElement);
+		
+		ROOT = messagesElement; //document.querySelector("section.messages");
+		
+		var actionListener = function(parentElement, subjectIdentity){
+		    PERSONID = subjectIdentity;
+		    SUBJECTID = subjectIdentity;
+		    ISPERSON = true;
+        	ROOT = parentElement;
+			this.displayRootMessages();
+			this.displayMessageEditor(parentElement, subjectIdentity);
+		};
 
-		this.refreshAvatarSlider(subjectsElement.querySelector("div.image-slider"), subjectIdentities, this.displayMessageEditor.bind(this, messagesElement));
+		this.refreshAvatarSlider(subjectsElement.querySelector("div.image-slider"), subjectIdentities, actionListener.bind(this, messagesElement));
 		this.displayRootMessages();
 	}
 
@@ -66,42 +84,92 @@ this.de_sb_messenger = this.de_sb_messenger || {};
         var messagesTime = [];
         var header = {"Accept": "application/json"};
         
-        var parentElement = document.querySelector("section.messages");
+        var parentElement = ROOT;
+        parentElement.innerHTML = '';
+        
+        var displayMessages = function(messages) {
+       		messages.sort((a, b) => a.creationTimestamp - b.creationTimestamp);
+			messages.forEach(function (message) {
+				console.log("Msg: ",message);
+	            var messagesOutput = document.querySelector("#message-output-template").content.cloneNode(true).firstElementChild;
+	            
+	            var outputs = messagesOutput.querySelectorAll("output");
+	            var outputSubject = outputs[0];
+	            var outputBody = outputs[1];
+	            var author_id = message.author.identity;
+
+	        	de_sb_util.AJAX.invoke("/services/people/" + author_id, "GET", header, null, null, function (request) {
+	        		var personGet = JSON.parse(request.responseText);
+	        		//console.log("Person: ",personGet);
+	        		outputSubject.innerHTML = personGet.name.given + " " + personGet.name.family + " ("+timeConverter(message.creationTimestamp)+")";
+	    			outputBody.innerHTML = message.body;
+	        	});
+	                
+	            messagesOutput.querySelector("img").src = "/services/people/" + author_id + "/avatar";
+	            
+	        	messagesOutput.querySelector("span").parentElement.addEventListener("click", function(){
+	        		var list = messagesOutput.querySelector("ul");
+	        		if(!list.hasChildNodes()){
+	        			ISPERSON = false;
+	        			SUBJECTID = message.identity;
+	        			ROOT = list;
+	        			messagesOutput.setAttribute("class","expanded");
+	        			self.displayRootMessages();
+	        			self.displayMessageEditor(list, message.identity);
+	        		}
+	        		else {
+	        			messagesOutput.setAttribute("class","message");
+	        			list.innerHTML = "";
+	        		}
+				});
+	            
+	            parentElement.appendChild(messagesOutput);
+	    	});
+        };
         
         de_sb_util.AJAX.invoke("/services/people/" + sessionUser.identity+"/messagesAuthored", "GET", header, null, null, function (request) {
             SUPER.prototype.displayStatus(request.status, request.statusText);
             if (request.status === 200) {
                 var messagesGet = JSON.parse(request.responseText);
-                messagesGet.sort((a, b) => a.creationTimestamp - b.creationTimestamp);
-                //console.log("all masgs: ",messagesGet);
+                //console.log("Your Msg: ",messagesGet);
                 messagesGet.forEach(function (message) {
-
-                    messages.push(message.identity);
-                    messagesTime.push(message.creationTimestamp)
-                    
-					//console.log("Msg: ",message);
-                    
-                    var messagesOutput = document.querySelector("#message-output-template").content.cloneNode(true).firstElementChild;
-                    
-                    var outputs = messagesOutput.getElementsByTagName("output");
-                    if(outputs.length>0){
-                    	var output = outputs[0];
-                    	de_sb_util.AJAX.invoke("/services/people/" + message.subject.identity, "GET", header, null, null, function (request) {
-                    		var personGet = JSON.parse(request.responseText);
-                    		//console.log("Person: ",personGet);
-                    		output.innerHTML=personGet.name.given + " " + personGet.name.family + " ("+timeConverter(message.creationTimestamp)+")<br>" + message.body;
-                    	});
-                    }
-                    
-                    var images = messagesOutput.getElementsByTagName("img");
-                    if(images.length>0){
-                    	images[0].src = "/services/people/" + message.subject.identity + "/avatar";
-                    }
-                    
-                    parentElement.appendChild(messagesOutput);
-                });
-            }
-        });
+					if(message.subject.identity === SUBJECTID){
+	                    messages.push(message);
+	                    messagesTime.push(message.creationTimestamp)
+	                    //console.log("Msg: ",message);
+	                }
+				});
+			}
+		
+			if(PERSONID != sessionUser.identity) {
+				var subjectId = (PERSONID === SUBJECTID) ? sessionUser.identity : SUBJECTID;
+				var messagesSubject = [];
+		        de_sb_util.AJAX.invoke("/services/people/" + PERSONID+"/messagesAuthored", "GET", header, null, null, function (request) {
+		            SUPER.prototype.displayStatus(request.status, request.statusText);
+		            if (request.status === 200) {
+		                var messagesGet = JSON.parse(request.responseText);
+		                //console.log("Subject msg: ",messagesGet);
+		                messagesGet.forEach(function (message) {
+							if(message.subject.identity === subjectId){
+			                    messagesSubject.push(message);
+			                }
+						});
+					}
+				
+					console.log("array1: ",messages);
+					console.log("array2: ",messagesSubject);
+					var combined = messages.concat(messagesSubject);
+					console.log("array3: ",combined);
+					displayMessages(combined);
+	
+				});
+				
+			}
+			else {
+				displayMessages(messages);
+			}
+  
+  		});
 
 		/*
 		sessionUser.observedReferences.forEach(function(identity){
@@ -142,51 +210,17 @@ this.de_sb_messenger = this.de_sb_messenger || {};
        	messagesInput.querySelector("button").addEventListener("click", function(){
 						self.sendMessage(subjectIdentity);
 				});
-		//messagesInput.id = "editor";
-        
-        parentElement.innerHTML = ''; // not sure if this will work in IE
-        //message.subject.identity
-        de_sb_util.AJAX.invoke("/services/people/" + sessionUser.identity+"/messagesAuthored", "GET", header, null, null, function (request) {
-            SUPER.prototype.displayStatus(request.status, request.statusText);
-            if (request.status === 200) {
-                var messagesGet = JSON.parse(request.responseText);
-                messagesGet.sort((a, b) => a.creationTimestamp - b.creationTimestamp);
-                
-                messagesGet.forEach(function (message) {
 
-                    if(message.subject.identity === subjectIdentity){
-                        var messagesOutput = document.querySelector("#message-output-template").content.cloneNode(true).firstElementChild;
-                        
-                        var outputs = messagesOutput.getElementsByTagName("output");
-                        if(outputs.length>0){
-                        	var output = outputs[0];
-                        	de_sb_util.AJAX.invoke("/services/people/" + message.subject.identity, "GET", header, null, null, function (request) {
-                        		var personGet = JSON.parse(request.responseText);
-                        		output.innerHTML=personGet.name.given + " " + personGet.name.family + " ("+timeConverter(message.creationTimestamp)+")<br>" + message.body;
-                        	});
-                        }
-                        
-                        var images = messagesOutput.getElementsByTagName("img");
-                        if(images.length>0){
-                        	images[0].src = "/services/people/" + message.subject.identity + "/avatar";
-                        }
-                        
-                        parentElement.appendChild(messagesOutput);
-                    }
-                });
-                
-				//var editor = document.querySelector("li.message[id='editor']"); // finds li message element with id 'editor'
-				
-                 var images = messagesInput.getElementsByTagName("img");
-                if(images.length>0){
-                	images[0].src = "/services/people/" + sessionUser.identity + "/avatar";
-                }
-				
-				//if(editor == null){
-	            	parentElement.appendChild(messagesInput);
-	            //}
-            }
-        });
+        messagesInput.querySelector("img").src = "/services/people/" + sessionUser.identity + "/avatar";
+		
+		var btn = document.querySelector("li.message button"); // only editor has button
+		
+        if(btn != null){
+        	var editor = btn.parentElement;
+        	editor.parentElement.removeChild(editor);
+        }
+        
+        parentElement.appendChild(messagesInput);
 	}
 
 	de_sb_messenger.MessagesController.prototype.sendMessage = function (subjectIdentity) {
@@ -201,7 +235,7 @@ this.de_sb_messenger = this.de_sb_messenger || {};
 					console.log("put msg",textMsg.value.trim() );
 				}
 		});
-			self.displayRootMessages();
+		self.displayRootMessages();
 	}
 
 
